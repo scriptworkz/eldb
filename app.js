2888b9471fd970dd4ff4d8eb6314c2fdb7f334bf
dbsops <117339402+dbsops@users.noreply.github.com>
2023-12-28 17:26:47 -0500

    initial update

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 
+app.get("/add", (req, res) => function() {
+  res.type('json').send({'status' => 200, 'message' => 'success'});
+});
+
 app.get("/", (req, res) => res.type('html').send(html));
 
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
